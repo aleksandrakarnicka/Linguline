c33fc3338665e73c08abd064bba7e6aefbe82abe
Joanna Maćkowiak <asia.mackowiak@gmail.com>
2021-05-18 23:33:12 +0200

    fix api

--- api.js
+++ api.js
@@ -1,22 +1,24 @@
-const APIword = document.getElementById("APIword")
-const APIpos =document.getElementById("partOfSpeech")
-word = APIword.value;
-let word
+function getSynonym(event) {
+    event.preventDefault()
+    const APIword = document.getElementById("final")
+    const APIpos =document.getElementById("partOfSpeech")
+    let word = APIword && APIword.value.split(' ')[0];
+    
+    let url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word;
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            APIword.innerHTML = "";
+            meanings = response[0] && response[0]["meanings"]
+            for(let i=0;i<meanings.lenght; i++) {
+                let li=document.createElement("li");
+                li.appendChild(document.createTextNode(meanings[i]));
+                APIword.appendChild(li);
+            }
 
-let url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word;
-fetch(url)
-.then(response => response.json())
-.then(response => {
-    APIword.innerHTML = "";
-    meanings = response[0]["meanings"]
-    for(let i=0;i<meanings.lenght; i++) {
-        let li=document.createElement("li");
-        li.appendChild(document.createTextNode(meanings[i]));
-        APIword.appendChild(li);
-    } 
-    partOfSpeech=response[0]["partOfSpeech"];
-    APIpos.innerHTML=partOfSpeech;
-    definition = response[0]["definition"];
-    synonyms = response[0]["synonyms"];
-    console.log(synonyms)
-})
+            partOfSpeech=meanings[0]["partOfSpeech"];
+            APIpos.innerHTML=partOfSpeech;
+            definition = meanings[0].definitions[0]["definition"];
+            synonyms = meanings[0].definitions[0]["synonyms"];
+    })
+}
