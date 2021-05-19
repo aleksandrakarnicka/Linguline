c1daafad9e4c68c7e12ad9a3a8055415da4c99a3
Marcin Fortuna <marcin.fortuna@ug.edu.pl>
2021-05-19 10:31:36 +0200

    Fix fetch; light refactor

--- api.js
+++ api.js
@@ -1,10 +1,12 @@
-function getSynonym(event) {
-    event.preventDefault()
-    const APIword = document.getElementById("final")
-    const APIpos =document.getElementById("partOfSpeech")
+function getDefinitions(event) {
+    event.preventDefault();
+    const APIword = document.getElementById("final");
     let word = APIword && APIword.value.split(' ')[0];
-    const output = document.getElementById("output")
+    
+    const output = document.getElementById("output");
+    
     let url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word;
+    
     fetch(url)
         .then(response => response.json())
         .then(response => {
@@ -13,28 +15,24 @@ function getSynonym(event) {
             meanings = response[0] && response[0]["meanings"]
             for(let i=0;i<meanings.length; i++) {
                 let newul = document.createElement("ul");
-                let meaning = meanings[i];
-                console.log(meaning)
+                // let meaning = meanings[i];
+                // console.log(meaning);
                 let li_pos=document.createElement("li");
                 li_pos.appendChild(document.createTextNode(meanings[i]["partOfSpeech"]));
                 output.appendChild(newul);
                 newul.appendChild(li_pos);
-                for(let j=0;j<meanings[i]["definitions"]; j++) {
+                for(let j=0;j<meanings[i]["definitions"].length; j++) {
                     let deful = document.createElement("ul");
                     let defli = document.createElement("li");
-                    let exli = document.createElement("li");
                     defli.appendChild(document.createTextNode(meanings[i]["definitions"][j]["definition"]));
-                    exli.appendChild(document.createTextNode(meanings[i]["definitions"][j]["example"]));
-                    console.log(meanings[i]["definitions"][j]["example"]);
                     output.appendChild(deful);
                     deful.appendChild(defli);
-                    deful.appendChild(exli);
+                    if (meanings[i]["definitions"][j]["example"]) {
+                        let exli = document.createElement("li");
+                        exli.appendChild(document.createTextNode(meanings[i]["definitions"][j]["example"]));
+                        deful.appendChild(exli);
+                    }
                 }
             }
-
-            partOfSpeech=meanings[0]["partOfSpeech"];
-            APIpos.innerHTML=partOfSpeech;
-            definition = meanings[0].definitions[0]["definition"];
-            synonyms = meanings[0].definitions[0]["synonyms"];
     })
 }
