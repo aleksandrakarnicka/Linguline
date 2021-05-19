2b282c151ed9d07df07de567c78d6bc3f3bb65af
aleksandrakarnicka <a.karnicka97@gmail.com>
2021-05-19 08:46:10 +0200

    fix api 2

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ function getSynonym(event) {
     const APIword = document.getElementById("final")
     const APIpos =document.getElementById("partOfSpeech")
     let word = APIword && APIword.value.split(' ')[0];
-    
+    const output = document.getElementById("output")
     let url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word;
     fetch(url)
         .then(response => response.json())
@@ -12,9 +12,24 @@ function getSynonym(event) {
             APIword.innerHTML = "";
             meanings = response[0] && response[0]["meanings"]
             for(let i=0;i<meanings.length; i++) {
-                let li=document.createElement("li");
-                li.appendChild(document.createTextNode(meanings[i]));
-                APIword.appendChild(li);
+                let newul = document.createElement("ul");
+                let meaning = meanings[i];
+                console.log(meaning)
+                let li_pos=document.createElement("li");
+                li_pos.appendChild(document.createTextNode(meanings[i]["partOfSpeech"]));
+                output.appendChild(newul);
+                newul.appendChild(li_pos);
+                for(let j=0;j<meanings[i]["definitions"]; j++) {
+                    let deful = document.createElement("ul");
+                    let defli = document.createElement("li");
+                    let exli = document.createElement("li");
+                    defli.appendChild(document.createTextNode(meanings[i]["definitions"][j]["definition"]));
+                    exli.appendChild(document.createTextNode(meanings[i]["definitions"][j]["example"]));
+                    console.log(meanings[i]["definitions"][j]["example"]);
+                    output.appendChild(deful);
+                    deful.appendChild(defli);
+                    deful.appendChild(exli);
+                }
             }
 
             partOfSpeech=meanings[0]["partOfSpeech"];
